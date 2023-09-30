45f3567e087abbbc4c74664349f619be6d7ed25a
Esteban Garviso <e.garvisovenegas@gmail.com>
2023-09-30 18:34:10 -0300

    feat: add healthcheck endpoint

---
+++ src/network/routes/healthcheck.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express'
+import response from './response'
+
+const HealthCheckRouter = Router()
+
+HealthCheckRouter.route('/healthcheck').get(async (req, res, next) => {
+  try {
+    response({
+      error: false,
+      message: 'Server is OK',
+      res,
+      status: 200
+    })
+  } catch (error) {
+    next(error)
+  }
+})
+
+/**
+ * @openapi
+ * /healthcheck:
+ *   get:
+ *     tags:
+ *       - Healthcheck
+ *     summary: Healthcheck
+ *     description: Healthcheck
+ *     responses:
+ *       200:
+ *         description: Healthcheck
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/HealthcheckResponse"
+ */
+export default HealthCheckRouter
